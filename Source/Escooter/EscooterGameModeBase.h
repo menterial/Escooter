34500e59c32b07dbd65b63ac344d676db64baf57
menterial <64th.ray@gmail.com>
2022-11-16 00:46:33 +0300

    initial commit
    Add clean project files

---
+++ Source/Escooter/EscooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCOOTER_API AEscooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
